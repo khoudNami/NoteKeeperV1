abb81364d7b46ad9c02fa430e2e711e053ff80df
Sbonelo <siboneloshezi50@gmail.com>
2020-06-10 18:16:38 +0200

    Stores the original note values from the ViewModel into the bundle. Module 6:Saving Activity State

--- app/src/main/java/com/zacademy/notekeeperv1/NoteActivity.java
+++ app/src/main/java/com/zacademy/notekeeperv1/NoteActivity.java
@@ -4,6 +4,7 @@ import android.content.ContentUris;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProvider;
@@ -117,6 +118,14 @@ public class NoteActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d("CALLED", "NoteActivity onDestroy() called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        if(outState!=null)
+            mViewModel.saveState(outState);
     }
 
     @Override
--- app/src/main/java/com/zacademy/notekeeperv1/NoteActivityViewModel.java
+++ app/src/main/java/com/zacademy/notekeeperv1/NoteActivityViewModel.java
@@ -1,9 +1,22 @@
 package com.zacademy.notekeeperv1;
 
+import android.os.Bundle;
+
 import androidx.lifecycle.ViewModel;
 
 public class NoteActivityViewModel extends ViewModel {
+    public static final String ORIGINAL_NOTE_COURSE_ID = "com.zacademy.notekeeperv1.ORIGINAL_NOTE_COURSE_ID";
+    public static final String ORIGINAL_NOTE_TITLE = "com.zacademy.notekeeperv1.ORIGINAL_NOTE_TITLE";
+    public static final String ORIGINAL_NOTE_TEXT = "com.zacademy.notekeeperv1.ORIGINAL_NOTE_TEXT";
+
+
     public String mOriginalNoteCourseId;
     public String mOriginalNoteTitle;
     public String mOriginalNoteText;
+
+    public void saveState(Bundle outState) {
+        outState.putString(ORIGINAL_NOTE_COURSE_ID, mOriginalNoteCourseId);
+        outState.putString(ORIGINAL_NOTE_TITLE, mOriginalNoteTitle);
+        outState.putString(ORIGINAL_NOTE_TEXT, mOriginalNoteText);
+    }
 }
