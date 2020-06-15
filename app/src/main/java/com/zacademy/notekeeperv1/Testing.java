0e39e74217701f98f39b35fb0ec0b512ed679446
Sbonelo <siboneloshezi50@gmail.com>
2020-06-15 08:41:11 +0200

    Working with Android Tools and Testing: Using JVM Testing : Implementing and Running a Unit Test. DataManagerTest class uses the assertEquals method to test if the values created by createNote() are actually stored after createNote() is invoked.

---
+++ app/src/main/java/com/zacademy/notekeeperv1/Testing.java
@@ -0,0 +1,13 @@
+package com.zacademy.notekeeperv1;
+
+public class Testing {
+
+
+    private String ownName;
+
+    public void method() {
+
+        ownName = "Refactoring using Ctrl+Alt+F";
+
+    }
+}
---
+++ app/src/test/java/com/zacademy/notekeeperv1/DataManagerTest.groovy
@@ -0,0 +1,11 @@
+package com.zacademy.notekeeperv1
+
+import org.junit.Test
+
+class DataManagerTest {
+    @Test
+    void testCreateNewNote() {
+
+
+    }
+}
