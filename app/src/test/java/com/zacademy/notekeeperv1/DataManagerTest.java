6ac2897c3e9bcaee89003810e80bb50914ddf6eb
Sbonelo <siboneloshezi50@gmail.com>
2020-06-16 08:46:23 +0200

    Working with Android Tools and Testing: Using JVM Testing : Implementing and Running a Unit Test. DataManagerTest class uses the assertSame to test if the note objects contain the same values. Order of parameters is important and should make sense.

--- app/src/test/java/com/zacademy/notekeeperv1/DataManagerTest.java
+++ app/src/test/java/com/zacademy/notekeeperv1/DataManagerTest.java
@@ -15,17 +15,17 @@ public class DataManagerTest {
         final String noteText = "This is the body of my test note";
 
         int noteIndex = dm.createNewNote();
-        NoteInfo newNote = dm.getNotes().get(noteIndex);
+        NoteInfo newNote = dm.getNotes().get(noteIndex - 1);
 
         newNote.setCourse(course);
         newNote.setTitle(noteTitle);
         newNote.setText(noteText);
 
-        NoteInfo compareNote = dm.getNotes().get(noteIndex);
-
-        assertEquals(compareNote.getCourse(), course);
-        assertEquals(compareNote.getTitle(), noteTitle);
-        assertEquals(compareNote.getText(), noteText);
+        NoteInfo compareNote = dm.getNotes().get(noteIndex - 1);
+// we expect to get back the first parameter, the second parameter is what we actually get back.
+        assertEquals(course, compareNote.getCourse());
+        assertEquals(noteTitle, compareNote.getTitle());
+        assertEquals(noteText, compareNote.getText());
 
     }
 }
