593a1a0287c768dcea1ccee20d1fd95ecc6571e8
Sbonelo <siboneloshezi50@gmail.com>
2020-06-18 21:06:07 +0200

    Understanding Instrumented Testing : Implementing Test Verification

--- app/src/androidTest/java/com/zacademy/notekeeperv1/NoteCreationTest.java
+++ app/src/androidTest/java/com/zacademy/notekeeperv1/NoteCreationTest.java
@@ -1,9 +1,12 @@
 package com.zacademy.notekeeperv1;
 
+import android.provider.ContactsContract;
+
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,10 +17,20 @@ import static androidx.test.espresso.Espresso.*;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static org.hamcrest.Matchers.*;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
 
 
 @RunWith(AndroidJUnit4.class)
 public class NoteCreationTest {
+
+    static DataManager sDataManager;
+
+    @BeforeClass
+    public static void classSetUp() {
+        sDataManager = DataManager.getInstance();
+    }
 
     @Rule
     public ActivityTestRule<NoteListActivity> mNoteListActivityRule =
@@ -25,14 +38,34 @@ public class NoteCreationTest {
 
     @Test
     public void createNewNote() {
+        final CourseInfo course = sDataManager.getCourse("java_lang");
+        String noteTitle = "Test note title";
+        String noteText = "This is the body of our test note";
+
 //        ViewInteraction fabNewNote = onView(withId(R.id.fab));
 //        fabNewNote.perform(click());
 
         onView(withId(R.id.fab)).perform(click());
-        onView(withId(R.id.text_note_title)).perform(typeText("Test note title"));
-        onView(withId(R.id.text_note_text)).perform(
-                typeText("This is the body of our test note"),
-                closeSoftKeyboard()
-        );
+
+        onView(withId(R.id.spinner_courses)).perform(click());
+        onData(allOf(instanceOf(CourseInfo.class), equalTo(course))).perform(click());
+        onView(withId(R.id.spinner_courses)).check(matches(withSpinnerText(
+                containsString(course.getTitle()))));
+
+        onView(withId(R.id.text_note_title)).perform(typeText(noteTitle))
+                .check(matches(withText(containsString(noteTitle))));
+
+        onView(withId(R.id.text_note_text))
+                .perform(typeText(noteText), closeSoftKeyboard());
+        onView(withId(R.id.text_note_text)).check(matches(withText(noteText)));
+
+        pressBack();
+
+        int noteIndex = sDataManager.getNotes().size() - 1;
+        NoteInfo note = sDataManager.getNotes().get(noteIndex);
+        assertEquals(course, note.getCourse());
+        assertEquals(noteTitle, note.getTitle());
+        assertEquals(noteText, note.getText());
+
     }
 }
