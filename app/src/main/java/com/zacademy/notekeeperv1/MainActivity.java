af4ea0ed98cbcccac4cfcb604c255dd9ec340e97
Sbonelo <siboneloshezi50@gmail.com>
2020-07-01 08:38:47 +0200

    Enhancing the Android Application Experience : Navigation Drawer: adds right navigation drawer.

--- app/src/main/java/com/zacademy/notekeeperv1/MainActivity.java
+++ app/src/main/java/com/zacademy/notekeeperv1/MainActivity.java
@@ -49,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
         NavigationView navigationView = findViewById(R.id.nav_view);
 
         // Passing each menu ID as a set of Ids because each
@@ -65,7 +66,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        mNoteRecyclerAdapter.notifyDataSetChanged();
+        mNoteRecyclerAdapter.notifyDataSetChanged();//notify adapter of any changes whenever we return
+        //to this activity
+
     }
 
     private void initializeDisplayContent() {
