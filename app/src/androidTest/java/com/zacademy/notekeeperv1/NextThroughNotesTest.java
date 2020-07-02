543373bfbea396d20154c9ea4e11d915fbda2292
Sbonelo <siboneloshezi50@gmail.com>
2020-07-02 12:52:57 +0200

    Enhancing the Android Application Experience : Testing Menu, NavigationDrawer and RecyclerView

---
+++ app/src/androidTest/java/com/zacademy/notekeeperv1/NextThroughNotesTest.java
@@ -0,0 +1,21 @@
+package com.zacademy.notekeeperv1;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NextThroughNotesTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> myActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void NextThroughNotes(){
+
+    }
+
+}
