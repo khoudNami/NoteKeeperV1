5251e470b2cdbf914415191bb4025c738feeb757
Sibonelo Shezi <siboneloshezi50@gmail.com>
2020-07-18 10:13:51 -0700

    Managing Android App Data with SQLite - Understanding SQLite and SQLite Database Creation - Adding Column Descriptions to the Contract Class

---
+++ app/src/main/java/com/zacademy/notekeeperv1/NoteKeeperDatabaseContract.java
@@ -0,0 +1,26 @@
+package com.zacademy.notekeeperv1;
+
+final public class NoteKeeperDatabaseContract {
+
+    public static final class CourseInfoEntry {
+        public static final String TABLE_NAME = "course_info";
+        public static final String COLUMN_COURSE_ID = "course_id";
+        public static final String COLUMN_COURSE_TITLE = "course_title";
+
+        //CREATE TABLE course_info (course_id, course_title)
+        public static final String SQL_CREATE_TABLE =
+                "CREATE TABLE " + TABLE_NAME + " (" + COLUMN_COURSE_ID + ", " + COLUMN_COURSE_TITLE + ")";
+    }
+
+    public static final class NoteInfoEntry {
+        public static final String TABLE_NAME = "note_info";
+        public static final String COLUMN_NOTE_TITLE = "note_title";
+        public static final String COLUMN_NOTE_TEXT = "note_text";
+        public static final String COLUMN_COURSE_ID = "course_id";
+
+        //CREATE TABLE note_info (note_title, note_text, course_id)
+        public static final String SQL_CREATE_TABLE =
+                "CREATE TABLE " + TABLE_NAME + " (" + COLUMN_NOTE_TITLE + ", " + COLUMN_NOTE_TEXT + ", " + COLUMN_COURSE_ID + ")";
+    }
+
+}
