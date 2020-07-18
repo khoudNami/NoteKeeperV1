64952fec0c0557842c52a8cfcb3057c5d5452a96
Sibonelo Shezi <siboneloshezi50@gmail.com>
2020-07-18 10:33:13 -0700

    Managing Android App Data with SQLite - Understanding SQLite and SQLite Database Creation - Making Your Database Framework Friendly, with Primary Keys

--- app/src/main/java/com/zacademy/notekeeperv1/NoteKeeperDatabaseContract.java
+++ app/src/main/java/com/zacademy/notekeeperv1/NoteKeeperDatabaseContract.java
@@ -1,18 +1,20 @@
 package com.zacademy.notekeeperv1;
 
+import android.provider.BaseColumns;
+
 final public class NoteKeeperDatabaseContract {
 
-    public static final class CourseInfoEntry {
+    public static final class CourseInfoEntry implements BaseColumns {
         public static final String TABLE_NAME = "course_info";
         public static final String COLUMN_COURSE_ID = "course_id";
         public static final String COLUMN_COURSE_TITLE = "course_title";
 
-        //CREATE TABLE course_info (course_id, course_title)
+        //CREATE TABLE course_info (_ID, course_id, course_title)
         public static final String SQL_CREATE_TABLE =
-                "CREATE TABLE " + TABLE_NAME + " (" + COLUMN_COURSE_ID + " TEXT UNIQUE NOT NULL, " + COLUMN_COURSE_TITLE + " TEXT NOT NULL)";
+                "CREATE TABLE " + TABLE_NAME + " (" + _ID + " INTEGER PRIMARY KEY, " + COLUMN_COURSE_ID + " TEXT UNIQUE NOT NULL, " + COLUMN_COURSE_TITLE + " TEXT NOT NULL)";
     }
 
-    public static final class NoteInfoEntry {
+    public static final class NoteInfoEntry implements BaseColumns {
         public static final String TABLE_NAME = "note_info";
         public static final String COLUMN_NOTE_TITLE = "note_title";
         public static final String COLUMN_NOTE_TEXT = "note_text";
@@ -20,7 +22,7 @@ final public class NoteKeeperDatabaseContract {
 
         //CREATE TABLE note_info (note_title, note_text, course_id)
         public static final String SQL_CREATE_TABLE =
-                "CREATE TABLE " + TABLE_NAME + " (" + COLUMN_NOTE_TITLE + " TEXT NOT NULL, " + COLUMN_NOTE_TEXT + " TEXT, " + COLUMN_COURSE_ID + " TEXT NOT NULL)";
+                "CREATE TABLE " + TABLE_NAME + " (" + _ID + " INTEGER PRIMARY KEY, " + COLUMN_NOTE_TITLE + " TEXT NOT NULL, " + COLUMN_NOTE_TEXT + " TEXT, " + COLUMN_COURSE_ID + " TEXT NOT NULL)";
     }
 
 }
