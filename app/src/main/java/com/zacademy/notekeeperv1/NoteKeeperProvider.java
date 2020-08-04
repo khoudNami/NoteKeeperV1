890914fce304443d328aeed0fb541bdf997db643
Sibonelo Shezi <siboneloshezi50@gmail.com>
2020-08-04 12:20:57 -0700

    Identifying Content Provider Content: add support for the new join relationship into ContentProvider query method

--- app/src/main/java/com/zacademy/notekeeperv1/NoteKeeperProvider.java
+++ app/src/main/java/com/zacademy/notekeeperv1/NoteKeeperProvider.java
@@ -20,9 +20,12 @@ public class NoteKeeperProvider extends ContentProvider {
 
     public static final int NOTES = 1;
 
+    public static final int NOTES_EXPANDED = 2;
+
     static { //static initializer runs some code when type is loaded
         sUriMatcher.addURI(NoteKeeperProviderContract.AUTHORITY, NoteKeeperProviderContract.Courses.PATH, COURSES);
         sUriMatcher.addURI(NoteKeeperProviderContract.AUTHORITY, NoteKeeperProviderContract.Notes.PATH, NOTES);
+        sUriMatcher.addURI(NoteKeeperProviderContract.AUTHORITY, NoteKeeperProviderContract.Notes.PATH_EXPANDED, NOTES_EXPANDED);
     }
 
     public NoteKeeperProvider() {
@@ -72,10 +75,26 @@ public class NoteKeeperProvider extends ContentProvider {
                 cursor = db.query(NoteInfoEntry.TABLE_NAME, projection, selection, selectionArgs,
                         null, null, sortOrder);
                 break;
+
+            case NOTES_EXPANDED:
+                cursor = notesExpandedQuery(db, projection, selection, selectionArgs, sortOrder);
+                break;
         }
 
-
         return cursor;
+    }
+
+    private Cursor notesExpandedQuery(SQLiteDatabase db, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+
+        //note_info JOIN course_info ON note_info.course_id = course_info.course_id
+        String tablesWithJoin = NoteInfoEntry.TABLE_NAME + " JOIN " +
+                CourseInfoEntry.TABLE_NAME + " ON " +
+                NoteInfoEntry.getQName(NoteInfoEntry.COLUMN_COURSE_ID) + " = " +
+                CourseInfoEntry.getQName(NoteInfoEntry.COLUMN_COURSE_ID);
+
+        return db.query(tablesWithJoin, projection,
+                null, null, null, null, sortOrder);
+
     }
 
     @Override
